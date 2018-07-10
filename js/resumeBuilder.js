7eeba413f624cd5f5ede0156a65861a6c5666506
hajczek <i.hajczewska@gmail.com>
2018-07-10 11:55:01 +0200

    Add +48 to mobile

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -6,7 +6,7 @@ const bio = {
     name: 'Iwona Kolanowska',
     role: 'Front-End Web Development',
     constacts: {
-        mobile: '605 832 505',
+        mobile: '+48 605 832 505',
         email: 'i.hajczewska@gmail.com',
         github: 'https://github.com/hajczek',
         location: 'Warsaw, Poland'
