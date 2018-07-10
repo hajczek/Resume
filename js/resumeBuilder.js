b0d96cf9501527d55104d8a52439cfc18fed43f1
hajczek <i.hajczewska@gmail.com>
2018-07-10 11:52:57 +0200

    Add object bio

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,20 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+
+const bio = {
+    name: 'Iwona Kolanowska',
+    role: 'Front-End Web Development',
+    constacts: {
+        mobile: '605 832 505',
+        email: 'i.hajczewska@gmail.com',
+        github: 'https://github.com/hajczek',
+        location: 'Warsaw'
+    },
+    welcomeMessage: 'Welcome on my Resume :)',
+    skills: [],
+    biopic: '',
+    display: function(){
+        
+    }    
+}
