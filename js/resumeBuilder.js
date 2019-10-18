5a077c9ce4b49c45190d38df7afd295718c251d7
Iwona Kolanowska <33376863+hajczek@users.noreply.github.com>
2019-10-18 19:22:46 +0200

    Changed description of welcomeMessage.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -11,7 +11,7 @@ const bio = {
             "github": "<a href='https://github.com/hajczek' target='_blank'>github.com/hajczek</a>",
             "location": "Poland, 05-822 Milanówek<br>Piasta 11"
         },
-        "welcomeMessage": "I'm self-employed in company 'Serwis stron'. I design and code websites. My career goal is to work as a FrontEnd Web Developer.",
+        "welcomeMessage": "I'm self-employed in company 'Serwis stron'. I design and code websites. My current career goal is to be a Front-End Developer in a professional Front-End team.",
         "skills": ["HTML5", "CSS3", "JavaScript", "jQuery", "React", "Git", "Grid", "Photoshop"],
         "biopic": "images/IK.png"   
     };
